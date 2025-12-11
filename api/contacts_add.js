56d853d82c88fba6b51259e004fc078d43dc5aaa
Yunxiuuu <yunxiu327@gmail.com>
2025-12-11 17:17:54 +0800

    Update contacts_add.js

--- api/contacts_add.js
+++ api/contacts_add.js
@@ -1,21 +1,13 @@
-const Contact = require("./contact-model");
-require("./mongo");
+import contacts from "./_db";
 
-module.exports = async (req, res) => {
-  try {
-    if (req.method !== "POST") {
-      return res.status(405).json({ error: "Method not allowed" });
-    }
+export default function handler(req, res) {
+  if (req.method !== "POST")
+    return res.status(405).send("Method Not Allowed");
 
-    if (!req.body.name || !req.body.phone) {
-      return res.status(400).json({ error: "Name and Phone required" });
-    }
+  const { name } = req.body;
+  const id = Date.now();
 
-    const contact = new Contact(req.body);
-    await contact.save();
+  contacts.push({ id, name });
 
-    return res.status(200).json(contact);
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
-  }
-};
+  res.json({ success: true });
+}
