e7d72001f4197fef782ffb1c37f8126e28d5f724
Yunxiuuu <yunxiu327@gmail.com>
2025-12-11 17:17:16 +0800

    Update contacts.js

--- api/contacts.js
+++ api/contacts.js
@@ -1,22 +1,5 @@
-const Contact = require("./contact-model");
-require("./mongo");
+import contacts from "./_db";
 
-module.exports = async (req, res) => {
-  try {
-    if (req.method !== "GET") {
-      return res.status(405).json({ error: "Method not allowed" });
-    }
-
-    const { name, phone, bookmark } = req.query;
-    let filter = {};
-
-    if (name) filter.name = { $regex: name, $options: "i" };
-    if (phone) filter.phone = { $regex: phone, $options: "i" };
-    if (bookmark === "true") filter.bookmark = true;
-
-    const contacts = await Contact.find(filter).sort({ name: 1 });
-    return res.status(200).json(contacts);
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
-  }
-};
+export default function handler(req, res) {
+  res.json(contacts);
+}
