ea32ab3ae8ad4aa062f71db925c611951048d815
Yunxiuuu <yunxiu327@gmail.com>
2025-12-11 15:54:45 +0800

    Delete api directory

--- api/index.js
+++
@@ -1,9 +0,0 @@
-// Vercel Serverless Function entrypoint.
-// Vercel will call files in /api as serverless functions.
-// This file forwards requests to the Express app exported from ../index.js
-const app = require('../index');
-
-module.exports = (req, res) => {
-  // app is an express app (callable handler)
-  return app(req, res);
-};
